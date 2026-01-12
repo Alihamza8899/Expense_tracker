f7aea8e887be41ce8bbf49bbe8d046dff7fcd95e
CodeSandbox <support@codesandbox.io>
2026-01-12 03:22:11 +0000

    Sandbox snapshot commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Navbar from "./Navbar.jsx";
+
 function App() {
   return (
-    <main className="flex flex-col items-center bg-gradient-to-br from-orange-300 via-orange-400 to-orange-600 max-w-full p-8 min-h-screen backdrop-blur-md">
-      <h1 className="text-4xl font-semibold tracking-wide text-white">
-        Task List App
-      </h1>
+    <main className=" bg-[#b8982a] max-w-full min-h-screen backdrop-blur-md">
+      <Navbar />
     </main>
   );
 }
---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+let Navbar = () => {
+  return (
+    <div className="bg-blue-700">
+      <h1>Expense Tracker</h1>
+      <ul>
+        <li>Home</li>
+        <li>About App</li>
+        <li>App features</li>
+        <li>Created By</li>
+      </ul>
+    </div>
+  );
+};
+export default Navbar;
