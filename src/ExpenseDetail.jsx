a5d16f3f98f2ff174bc182dd067699ff213bdc2b
CodeSandbox <support@codesandbox.io>
2026-01-16 15:10:59 +0000

    Sandbox snapshot commit

---
+++ src/ExpenseDetail.jsx
@@ -0,0 +1,10 @@
+let ExpenseDetail = () => {
+  return (
+    <div className="flex justify-between my-2 p-2 bg-gray-200">
+      <h1>expense name</h1>
+      <h1>expense value</h1>
+      <button>&#times;</button>
+    </div>
+  );
+};
+export default ExpenseDetail;
--- src/Expensebox.jsx
+++ src/Expensebox.jsx
@@ -1,3 +1,5 @@
+import ExpenseDetail from "./ExpenseDetail";
+
 let Expensebox = () => {
   return (
     <div className="min-w-[40%] border border-white rounded-lg min-h-[200px] bg-white p-4 shadow-[8px_8px_9px_rgba(0,0,0,0.1)]">
@@ -15,9 +17,17 @@ let Expensebox = () => {
             className="mx-2 border border-[rgba(0,0,0,0.4)] p-2 rounded-md focus:ring-2 focus:ring-[#b8982a] focus:outline-[#b8982a]"
           />
         </div>
-        <button type="submit" className="w-full rounded-md bg-[#101a37] py-2 text-white">Add</button>
+        <button
+          type="submit"
+          className="w-full rounded-md bg-[#101a37] py-2 text-white"
+        >
+          Add
+        </button>
       </form>
-      div
+      <div className="my-4">
+        <hr className="border border-gray-300" />
+        <ExpenseDetail />
+      </div>
     </div>
   );
 };
