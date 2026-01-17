1ed7816c708d2a7e91100b6f83734a6f006a006d
CodeSandbox <support@codesandbox.io>
2026-01-17 04:33:56 +0000

    Sandbox snapshot commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,13 @@ function App() {
       <div className="flex justify-center px-2 py-16 bg-[#dcdcdc] min-h-[35rem]">
         <Expensebox />
       </div>
-      <div className="h-[26.5rem] w-full bg-[#101a37] p-4 shadow shadow-[#101a37]">
+      <div className="min-h-[26.5rem] w-full bg-[#101a37] p-4 shadow shadow-[#101a37]">
         <AboutApp />
       </div>
-      <div className="w-full h-[25rem] bg-[#dcdcdc] p-4">
+      <div className="w-full min-h-[25rem] bg-[#dcdcdc] p-4">
         <AppFeatures />
       </div>
-      <div className="w-full h-[20rem] bg-[#101a37]">
+      <div className="w-full min-h-[20rem] bg-[#101a37]">
         <CreatedBy />
       </div>
     </main>
